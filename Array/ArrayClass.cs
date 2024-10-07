a7b6dceacb735d2d849006c41e731e74b8837861
ajinkya29git <jadhavajinkya29git@gmail.com>
2024-10-07 20:38:32 +0530

    All sub arrays of the given array

---
+++ Array/ArrayClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class ArrayClass
+{
+    public static int[] MyArray {get; private set;}
+    
+    static ArrayClass()
+    {
+        MyArray = new int[5] {1,2,3,4,5};
+    }
+
+    public static void printAllSubArrays(int[] arr)
+    {
+        int n = arr.Length;
+
+        for (int length = 1; length <= n; length++) // Start from length 2 to n
+        {
+            for (int i = 0; i <= n - length; i++)
+            {
+                for (int j = i; j < i + length; j++)
+                { 
+                    Console.Write(arr[j]+ " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        printAllSubArrays(MyArray);
+    }
+
+}
